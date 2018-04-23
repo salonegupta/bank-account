4a790e676b0a280c90d06ef421ca28788fc1aade
Salone Gupta <salone.gupta@urbanladder.com>
2018-04-23 10:35:02 +0530

    Update the controller route for swagger documentation

--- src/main/java/com/banking/SwaggerConfig.java
+++ src/main/java/com/banking/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.banking.rules.controller"))
+				.apis(RequestHandlerSelectors.basePackage("com.banking.controller"))
 				.paths(PathSelectors.any()).build();
 	}
 }
